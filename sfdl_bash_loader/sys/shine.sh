3fc939f2fd71e35c71629c80b0432990c3f16678
JobbeDeluxe <jobbedeluxe@gmail.com>
2018-08-03 14:50:15 +0200

    Update shine.sh

--- sfdl_bash_loader/sys/shine.sh
+++ sfdl_bash_loader/sys/shine.sh
@@ -2,6 +2,8 @@
 
 pwd="`dirname \"$0\"`"
 source "$pwd/kategorie.cfg"
+
+echo "$kat"
 if [ -z "$tmdb_filmdatei" ]; then
 	tmdb_filmdatei="$(find "$sfdl_downloads/$name/" -type f | xargs -d '\n' ls -S | head -1)"
 	film_extension="${tmdb_filmdatei##*.}"
@@ -10,7 +12,7 @@ if [ -z "$tmdb_filmdatei" ]; then
 	tmdb_filmdatei="$sfdl_downloads/$name/$dateiname.$film_extension"
 fi
 if [ $kategorie == true ]; then
-	if [ $kat == film ]; then
+	if [ $kat == "film" ]; then
 		if [ $unterordner == false ]; then
 			mv "$tmdb_filmdatei" "$sfdl_downloads/$Film/$film_ganzefilm"
 			mv "$sfdl_downloads/$name/speedreport.txt" "$sfdl_downloads/"$name"_speedreport.txt"
