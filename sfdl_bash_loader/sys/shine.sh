0e408c1f90ba91f797893d1a08f135360a1b9a6a
JobbeDeluxe <jobbedeluxe@gmail.com>
2018-08-31 11:20:59 +0200

    Update shine.sh

--- sfdl_bash_loader/sys/shine.sh
+++ sfdl_bash_loader/sys/shine.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 
-pwd="`dirname \"$0\"`"
 source "$pwd/kategorie.cfg"
-if [ -z "$tmdb_filmdatei" ]; then
 	tmdb_filmdatei="$(find "$sfdl_downloads/$name/" -type f | xargs -d '\n' ls -S | head -1)"
 	film_extension="${tmdb_filmdatei##*.}"
 	film_ganzefilm="${tmdb_filmdatei##*/}"
-	else
-	tmdb_filmdatei="$sfdl_downloads/$name/$dateiname.$film_extension"
-fi
+katname=kat_$kat
+
 if [ $kategorie == true ]; then
-	if [ $kat == "film" ]; then
-		if [ $unterordner == false ]; then
-			mv "$tmdb_filmdatei" "$sfdl_downloads/$Film/$film_ganzefilm" ###der name stimmt noch nicht... neue datei suche??
-			mv "$sfdl_downloads/$name/speedreport.txt" "$sfdl_downloads/"$name"_speedreport.txt"
-			if [ $removeold == true ]; then			
-				rm -dr $sfdl_downloads/$name
-			fi
-		else
-			mv "$sfdl_downloads/$name" "$sfdl_downloads/$Film/$name"
+	if [ $unterordner == false ]; then
+		mv "$tmdb_filmdatei" "$sfdl_downloads/${!kategorie}/$film_ganzefilm"
+		mv "$sfdl_downloads/$name/speedreport.txt" "$sfdl_downloads/"$name"_speedreport.txt"
+		if [ $removeold == true ]; then			
+			rm -dr $sfdl_downloads/$name
 		fi
+	else
+		mv "$sfdl_downloads/$name" "$sfdl_downloads/${!kategorie}/$name"
 	fi
 fi
