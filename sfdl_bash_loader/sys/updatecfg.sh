639a22aaa60c5d9d97fbf3b7224814bdd67fc736
JobbeDeluxe <jobbedeluxe@gmail.com>
2018-09-03 11:28:43 +0200

    Update updatecfg.sh
    add sample remove

--- sfdl_bash_loader/sys/updatecfg.sh
+++ sfdl_bash_loader/sys/updatecfg.sh
@@ -24,6 +24,7 @@ sfdl_chmod=$sfdl_chmod						# Rechte verteilung der Daten nach und während dem 
 sfdl_update=$sfdl_update						# ask = fragen, true = immer updaten, false = nicht updaten
 History=$History						# true = es wird in /logs/History.txt der Download verlauf mit Datum angelegt.
 kategorie=$kategorie						#Automatisches sortieren von Filmen und Serein siehe kategorie.cfg
+sample_remove=$sample_remove        #entfernt alle daten im Download ordner die das wort "sample" enthalten. (achtung bei filmen oder sereien die eventuell einen namen mit sample haben.
 #==========================================================================================================
 uscript_folder=\"$uscript_folder\"   			#absoluter Pfad zu script Ordner
 uscript_befor=$uscript_befor               			#Userscript vor dem Download ausführen. true zum aktivieren
